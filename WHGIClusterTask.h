5984a64d7ca36ee35b2ee864bb091b05b0468d1c
徐俊峰 <xjf1990930@163.com>
2019-11-07 10:07:52 +0800

    Update WHGIClusterTask.h

--- WHGIClusterTask.h
+++ WHGIClusterTask.h
@@ -14,8 +14,8 @@
 @class AGSSpatialReference;
 
 //负责数据查询
-#define pWHGIClustLayerService [WHGIDataSwitch mapDynamicLayerURLForType:[WHGIDataSwitch currentType]]
-#define pWHGIClustLayerIDs @[@"0",@"2",@"3",@"4",@"6",@"7",@"8"]
+#define pWHGIClustLayerService @"图层服务地址"
+#define pWHGIClustLayerIDs @[@"0",@"2",@"3",@"4",@"6",@"7",@"8"]//需要被聚合的图层id
 #define pWHGIClustLayerSymbolImageNames @[@"单位信息",@"社会消防力量",@"消防机构",@"熟悉演练",@"消防栓",@"取水码头",@"天然水源"];
 
 @interface WHGIClusterTask : NSObject
